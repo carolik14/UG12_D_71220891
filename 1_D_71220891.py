b8fcf3722a9fafe2c8d3efb2920f83743bcf7533
carolik14 <117900960+carolik14@users.noreply.github.com>
2022-12-08 16:06:15 +0700

    Add files via upload

---
+++ 1_D_71220891.py
@@ -0,0 +1,4 @@
+input("Masukkan jumlah uang: Rp")
+input("Ketik 'START' untuk memulai: ")
+input("Apa barang yang akakn anda beli? ")
+print("sisa uang anda ")
---
+++ 2_D_71220891.py
@@ -0,0 +1,13 @@
+print("~~~~~~~~ Tabel Matematika Nich ~~~~~~~~")
+print("Pilihan Model Matematika")
+pila = print("1. Perkalian")
+pilb = print("2. Pembagian")
+a = input("Masukkan model matematika yang diinginkan 1/2: ")
+c = int(input("Menampilkan table matematika dari angka: "))
+count = 3
+for i in range(1, count + 1):
+    print(i, "x", count, == i * "3")
+
+
+# if a == pila:
+#     print(c, "x", angka, "=")
